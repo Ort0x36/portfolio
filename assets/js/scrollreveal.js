e566c2305c7e9a26301c3433396e8a6b9c9abb01
0ortiz <wsortiz@outlook.com>
2022-11-01 11:28:39 -0300

    updates and adds

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.img-box', {duration : 3000, origin: 'right'})
+sr.reveal('.content-head', {duration : 3000, origin: 'left'})
+sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
+sr.reveal('.contrib-projetos', {duration : 3000, origin:'bottom'})
+sr.reveal('.projetos-sc', {duration : 3000, origin:'bottom'})
+sr.reveal('.end', {duration : 3000, origin:'left'})
+sr.reveal('.livros-sc', {duration : 3000, origin:'right'})
