97c2916bd07284a8b3e1860a90d5188b916e4d44
0ortiz <wsortiz@outlook.com>
2022-11-06 17:14:55 -0300

    updates

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -1,10 +1,10 @@
 window.sr = ScrollReveal({ reset:true});
 ScrollReveal({ distance: '50px' });
-sr.reveal('.img-box', {duration : 3000, origin: 'left'})
+sr.reveal('.img-box', {duration : 3000, origin: 'right'})
 sr.reveal('.content-head', {duration : 3000, origin: 'left'})
 sr.reveal('.darkmode-input', {duration : 3000, origin: 'left'})
-sr.reveal('.sobre-mim', {duration : 3000, origin:'left'})
+sr.reveal('.sobre-mim', {duration : 3000, origin:'right'})
 sr.reveal('.projetos-sc', {duration : 3000, origin:'bottom'})
 sr.reveal('.contrib-projetos', {duration : 3000, origin:'bottom'})
-sr.reveal('.livros-sc', {duration : 3000, origin:'left'})
-sr.reveal('.end', {duration : 3000, origin:'bottom'})
+sr.reveal('.livros-sc', {duration : 3000, origin:'right'})
+sr.reveal('.end', {duration : 3000, origin:'left'})
