9fcd90b69260105da60747d264589696c8619b14
Wendel Ortiz <wendel.ortiz.dev@gmail.com>
2025-01-08 20:43:24 -0300

    update

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,6 +1,11 @@
 const chk = document.querySelector("#chk");
-
+const twitter = document.getElementById("x");
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
+    if (document.body.classList.contains('dark')) {
+        twitter.style.color = "white";
+    } else {
+        twitter.style.color = "black"; 
+    }
 });
